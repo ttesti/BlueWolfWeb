1a103440c6cbb44bb58c8e2a2b310e045307ebd3
ttesti <tmtesti@gmail.com>
2023-08-16 09:02:15 -0500

    First commit of BlueWolfApp

---
+++ BlueWolfRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BlueWolfRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; } = default!;
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int Displayorder { get; set; }
+    }
+}
