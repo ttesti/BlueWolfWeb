7f9c783019056af4b163260afbb8f3aa4b071f36
ttesti <tmtesti@gmail.com>
2023-08-16 14:59:23 -0500

    Fixed the footer issue where the footer was at the top of the screen

--- BlueWolf.Utilites/obj/Debug/net8.0/BlueWolf.Utilites.AssemblyInfo.cs
+++ BlueWolf.Utilites/obj/Debug/net8.0/BlueWolf.Utilites.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlueWolf.Utilites")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17fbad582401106b915836575b8240842e17bab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlueWolf.Utilites")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlueWolf.Utilites")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
