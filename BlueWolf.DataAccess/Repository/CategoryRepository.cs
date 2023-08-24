88074e27d3da94edde11b7730aea8f54f65c55fd
ttesti <tmtesti@gmail.com>
2023-08-24 16:24:48 -0500

    2023-24-08: hooked up the data access for N-Tier environment

---
+++ BlueWolf.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using BlueWolf.Data;
+using BlueWolf.DataAccess.Repository.IRepository;
+using BlueWolf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueWolf.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ BlueWolf.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using BlueWolf.DataAccess.Repository.IRepository;
+using BlueWolf.Data;
+
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueWolf.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork 
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category{ get;private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        //public ICategoryRepository Category { get; set; }
+        //ApplicationDbContext Db { get; set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+       
+    }
+}
